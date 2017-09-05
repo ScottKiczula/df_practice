855c53ebc0440734f53384b8d7875b6f091fb004
ScottKiczula <kiczulascott@gmail.com>
2017-09-05 08:56:57 -0500

    2 tests for each model 3 failing.

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, :type => :model do
-  it "is valid with valid attributes" 
-  it "is not valid without a name"
+  it "is valid with valid attributes" do
+    expect(Reservation.new).to be_valid
+  end
+  it "is not valid without a name" do
+    resetvation = Reservation.new(name: nil)
+    expect(reservation).to_not be_valid
+  end
 end
--- spec/models/table_spec.rb
+++ spec/models/table_spec.rb
@@ -1,4 +1,9 @@
 RSpec.describe Table, :type => :model do
-  it "is valid with valid attributes" 
-  it "is not valid without a number of seats"
+  it "is valid with valid attributes" do
+    expect(Table.new).to be_valid
+  end
+  it "is not valid without a number of seats" do
+    table = Table.new(seats: nil)
+    expect(table).to_not be_valid
+  end
 end
