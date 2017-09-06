920b13c1ccc43c398ed698a785ce09eda5df9dd7
ScottKiczula <kiczulascott@gmail.com>
2017-09-05 23:50:27 -0500

    Made all but one method private.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -10,27 +10,28 @@ class Reservation < ActiveRecord::Base
     self.table_id = find_table_for_reservation
   end
 
-  def reserved_tables
-    unavailable_tables = []
-    matching_reservation_times = Reservation.where(time: time)
-    matching_reservation_times.each{|reservation| unavailable_tables << reservation.table_id}
-    unavailable_tables
-  end
-
-  def available_tables
-    all_tables = Table.all
-    table_collection = []
-    all_tables.each{|table| table_collection << table.id}
-    table_collection - reserved_tables
-  end
-
-  def find_table_for_reservation
-    available_tables.each do |table|
-      possible_match = Table.find(table)
-      if possible_match.seats >= self.guests
-        return possible_match.id
-      end
+  private
+    def reserved_tables
+      unavailable_tables = []
+      matching_reservation_times = Reservation.where(time: time)
+      matching_reservation_times.each{|reservation| unavailable_tables << reservation.table_id}
+      unavailable_tables
     end
-    self.errors.add(:table_id, "There is not enough seats for your party.")
-  end
+
+    def available_tables
+      all_tables = Table.all
+      table_collection = []
+      all_tables.each{|table| table_collection << table.id}
+      table_collection - reserved_tables
+    end
+
+    def find_table_for_reservation
+      available_tables.each do |table|
+        possible_match = Table.find(table)
+        if possible_match.seats >= self.guests
+          return possible_match.id
+        end
+      end
+      self.errors.add(:table_id, "There is not enough seats for your party.")
+    end
 end
